ff4650f280dc1294067c1fa99709147ac63d22ae
Tony Hernandez <tony@sctr.net>
2018-04-11 08:01:07 -0700

    Fixed deprecated ConsoleExceptionEvent

--- src/M6WebStatsdBundle.php
+++ src/M6WebStatsdBundle.php
@@ -2,6 +2,8 @@
 
 namespace M6Web\Bundle\StatsdBundle;
 
+use M6Web\Bundle\StatsdBundle\DependencyInjection\M6WebStatsdExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -13,10 +15,14 @@ class M6WebStatsdBundle extends Bundle
      * trick allowing bypassing the Bundle::getContainerExtension check on getAlias
      * not very clean, to investigate
      *
-     * @return Object DependencyInjection\M6WebStatsdExtension
+     * @return M6WebStatsdExtension|ExtensionInterface
      */
     public function getContainerExtension()
     {
-        return new DependencyInjection\M6WebStatsdExtension();
+        if (null === $this->extension) {
+            $this->extension = new M6WebStatsdExtension();
+        }
+
+        return $this->extension;
     }
 }
--- src/Tests/M6WebTestingKernel.php
+++ src/Tests/M6WebTestingKernel.php
@@ -2,11 +2,24 @@
 
 namespace M6Web\Bundle\StatsdBundle\Tests;
 
+use M6Web\Bundle\StatsdBundle\DependencyInjection\M6WebStatsdExtension as BaseM6WebStatsdExtension;
 use M6Web\Bundle\StatsdBundle\M6WebStatsdBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpKernel\Kernel;
 
 class M6WebTestingKernel extends Kernel
 {
+    private $m6WebStatsdConfig;
+
+    public function __construct(array $m6WebStatsdConfig = [])
+    {
+        $this->m6WebStatsdConfig = $m6WebStatsdConfig;
+
+        parent::__construct('test', true);
+    }
+
     public function registerBundles()
     {
         return [
@@ -16,7 +29,10 @@ class M6WebTestingKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        // TODO: Implement registerContainerConfiguration() method.
+        $loader->load(function(ContainerBuilder $container) {
+            $container->register('event_dispatcher', new EventDispatcher());
+            $container->registerExtension(new BaseM6WebStatsdExtension());
+            $container->loadFromExtension('m6_statsd');
+        });
     }
-
 }
