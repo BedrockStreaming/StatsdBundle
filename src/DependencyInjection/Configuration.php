c1d928a86a346eee5026c3741840da231a7f909d
Oliver THEBAULT <Oliboy50@users.noreply.github.com>
2020-07-06 16:25:36 +0200

    fix(review): remove unused use statement

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,7 +4,6 @@ namespace M6Web\Bundle\StatsdBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
-use Symfony\Component\HttpKernel\Kernel;
 
 /**
  * This is the class that validates and merges configuration from your app/config files
